cffd05b20307edd7ec5094cc407c25c68396a0d3
I045234 <nit.agrawal@sap.com>
2018-05-12 14:58:29 +0530

    basic maven app

---
+++ basicmvnapp/src/main/java/com/niaa/App.java
@@ -0,0 +1,13 @@
+package com.niaa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
